eae4bf9e46d42ba61a14046d36f84e2eb9452975
OmkarMahajan <mahajanomu@gmail.com>
2019-01-11 23:51:12 +0530

    Layout and listener added

---
+++ MainActivity.java
@@ -0,0 +1,49 @@
+package com.londonappbrewery.xylophonepm;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  {
+
+    // Helpful Constants
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+    // TODO: Add member variables here
+    private SoundPool mSoundpool;
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mASoundId;
+    private int mBSoundId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: Create a new SoundPool
+        mSoundpool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC,0);
+
+        // TODO: Load and get the IDs to identify the sounds
+
+        //mCSoundId = mSoundpool.load(MainActivity.this,0,0);
+    }
+
+    // TODO: Add the play methods triggered by the buttons
+   public void cPlay(View v)
+   {
+       Log.d("hi","Succeessfull");
+   }
+}
